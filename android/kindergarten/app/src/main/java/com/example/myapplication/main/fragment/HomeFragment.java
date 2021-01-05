87d73db36a785e6deb2ee2829d59af74e31d7adb
李晓璐 <44945785+LXLDEMO@users.noreply.github.com>
2021-01-05 18:44:52 +0800

    update the client - teacher part

--- android/kindergarten/app/src/main/java/com/example/myapplication/main/fragment/HomeFragment.java
+++ android/kindergarten/app/src/main/java/com/example/myapplication/main/fragment/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.myapplication.main.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +14,20 @@ import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
 import com.example.myapplication.apply.activity.ApplyActivityBaby;
+import com.example.myapplication.main.util.ConfigUtil;
 import com.example.myapplication.more.activity.MoreActivity;
 import com.example.myapplication.school.activity.SchoolActivity;
 import com.example.myapplication.teacher.activity.TeacherActivity;
 import com.youth.banner.Banner;
 import com.youth.banner.listener.OnBannerListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HomeFragment extends Fragment implements OnBannerListener {
 
@@ -79,9 +89,32 @@ public class HomeFragment extends Fragment implements OnBannerListener {
                     startActivity(intent1);
                     break;
                 case R.id.linear_to_teacher:
-                    Intent intent2 = new Intent();
-                    intent2.setClass(getContext(), TeacherActivity.class);
-                    startActivity(intent2);
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            //发送到服务端
+                            try {
+                                URL url = new URL(ConfigUtil.SERVICE_ADDRESS + "getAllTeacherMessage.action" );
+                                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                                conn.setRequestMethod("POST");
+                                //获取网络输入流
+                                InputStream in = conn.getInputStream();
+                                //使用字符流读取
+                                BufferedReader reader = new BufferedReader(new InputStreamReader(in, "utf-8"));
+                                //读取返回的数据 json串
+                                String results = reader.readLine();
+
+                                Intent intent = new Intent(getContext(), TeacherActivity.class);
+                                intent.putExtra("results", results);
+                                Log.e("results", results);
+                                startActivity(intent);
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }.start();
                     break;
                 case R.id.linear_to_more:
                     Intent intent3 = new Intent();

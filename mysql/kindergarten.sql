585811a16a6035bfd7394a4de6d4d720666a36c9
liurufeng666 <2221336136@qq.com>
2021-01-21 11:10:50 +0800

    lrf update database

--- mysql/kindergarten.sql
+++ mysql/kindergarten.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost_3306
-Source Server Version : 50701
+Source Server Version : 50506
 Source Host           : localhost:3306
 Source Database       : kindergarten
 
 Target Server Type    : MYSQL
-Target Server Version : 50701
+Target Server Version : 50506
 File Encoding         : 65001
 
-Date: 2021-01-19 23:48:26
+Date: 2021-01-21 11:05:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -119,17 +119,16 @@ CREATE TABLE `kindergarten_environment_description` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(600) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of kindergarten_environment_description
 -- ----------------------------
-INSERT INTO `kindergarten_environment_description` VALUES ('1', '环境优美\\n空气清新\\n阳光充足\\n宽敞的操场\\n硕果累累');
-INSERT INTO `kindergarten_environment_description` VALUES ('2', '充满\\n文艺气息\\n的楼道');
-INSERT INTO `kindergarten_environment_description` VALUES ('3', '整洁的\\n室内环境');
-INSERT INTO `kindergarten_environment_description` VALUES ('4', '我园配备了\\n大型玩具\\n游乐设施\\n供幼儿玩耍');
-INSERT INTO `kindergarten_environment_description` VALUES ('5', '非常注重\\n幼儿动手、\\n动脑能力\\n发展幼儿思维\\n为此\\n设立了活动区域非常注重\\n幼儿动手、\\n动脑能力\\n发展幼儿思维\\n为此\\n设立了活动区域');
-INSERT INTO `kindergarten_environment_description` VALUES ('6', '有利于幼儿\\n积极动脑\\n大胆创作\\n学会友好合作\\n互帮互助\\n促进幼儿\\n良好个性发展');
+INSERT INTO `kindergarten_environment_description` VALUES ('1', '我园环境优美，空气清新，阳光充足。\r\n宽敞的操场，硕果累累：');
+INSERT INTO `kindergarten_environment_description` VALUES ('2', '充满文艺气息的楼道');
+INSERT INTO `kindergarten_environment_description` VALUES ('3', '整洁的室内环境');
+INSERT INTO `kindergarten_environment_description` VALUES ('4', '好玩的大滑梯（我园配备了大型玩具游乐设施供幼儿玩耍）');
+INSERT INTO `kindergarten_environment_description` VALUES ('5', '温馨的区角：我园非常注重幼儿动手，动脑能力，发展幼儿思维，为此设立了活动区域');
 
 -- ----------------------------
 -- Table structure for `kindergarten_environment_picture`
@@ -155,20 +154,20 @@ INSERT INTO `kindergarten_environment_picture` VALUES ('7', 'wall2.png', '2');
 INSERT INTO `kindergarten_environment_picture` VALUES ('8', 'desk.png', '3');
 INSERT INTO `kindergarten_environment_picture` VALUES ('9', 'drawer.png', '3');
 INSERT INTO `kindergarten_environment_picture` VALUES ('10', 'play1.png', '4');
-INSERT INTO `kindergarten_environment_picture` VALUES ('11', 'play2.png', '0');
+INSERT INTO `kindergarten_environment_picture` VALUES ('11', 'play2.png', '4');
 INSERT INTO `kindergarten_environment_picture` VALUES ('12', 'play3.png', '4');
 INSERT INTO `kindergarten_environment_picture` VALUES ('13', 'play4.png', '4');
-INSERT INTO `kindergarten_environment_picture` VALUES ('14', 'angle1.png', '6');
+INSERT INTO `kindergarten_environment_picture` VALUES ('14', 'angle1.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('15', 'angle2.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('16', 'angle3.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('17', 'angle4.png', '5');
-INSERT INTO `kindergarten_environment_picture` VALUES ('18', 'angle5.png', '0');
-INSERT INTO `kindergarten_environment_picture` VALUES ('19', 'angle6.png', '0');
+INSERT INTO `kindergarten_environment_picture` VALUES ('18', 'angle5.png', '5');
+INSERT INTO `kindergarten_environment_picture` VALUES ('19', 'angle6.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('20', 'angle7.png', '5');
-INSERT INTO `kindergarten_environment_picture` VALUES ('21', 'angle8.png', '6');
-INSERT INTO `kindergarten_environment_picture` VALUES ('22', 'angle9.png', '6');
-INSERT INTO `kindergarten_environment_picture` VALUES ('23', 'angle10.png', '6');
-INSERT INTO `kindergarten_environment_picture` VALUES ('24', 'angle11.png', '6');
+INSERT INTO `kindergarten_environment_picture` VALUES ('21', 'angle8.png', '5');
+INSERT INTO `kindergarten_environment_picture` VALUES ('22', 'angle9.png', '5');
+INSERT INTO `kindergarten_environment_picture` VALUES ('23', 'angle10.png', '5');
+INSERT INTO `kindergarten_environment_picture` VALUES ('24', 'angle11.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('25', 'angle12.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('26', 'angle13.png', '5');
 INSERT INTO `kindergarten_environment_picture` VALUES ('27', 'angle14.png', '5');
@@ -224,6 +223,25 @@ CREATE TABLE `kindergarten_phone` (
 INSERT INTO `kindergarten_phone` VALUES ('1', '0311-858857');
 
 -- ----------------------------
+-- Table structure for `parent`
+-- ----------------------------
+DROP TABLE IF EXISTS `parent`;
+CREATE TABLE `parent` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` char(11) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `nickname` varchar(20) NOT NULL,
+  `avatar` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of parent
+-- ----------------------------
+INSERT INTO `parent` VALUES ('1', '18831166551', '7777777', '七道', null);
+INSERT INTO `parent` VALUES ('2', '18831166552', '77777772', '七道二', null);
+
+-- ----------------------------
 -- Table structure for `relation`
 -- ----------------------------
 DROP TABLE IF EXISTS `relation`;
@@ -250,39 +268,40 @@ CREATE TABLE `teacher` (
   `phone` varchar(11) NOT NULL,
   `picture` varchar(100) DEFAULT NULL,
   `motto` varchar(200) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of teacher
 -- ----------------------------
-INSERT INTO `teacher` VALUES ('1', '贾静宇', '园长', '18732184631', 'teacherImgs/jiajingyu.jpg', '爱就是教育，没有爱便没有教育；不求尽善尽美，但求无愧于心。');
-INSERT INTO `teacher` VALUES ('2', '周萍', '后勤主任', '15832138253', 'teacherImgs/zhouping.jpg', '用我的细心、耐心和爱心，换你的安心、放心和舒心。');
-INSERT INTO `teacher` VALUES ('3', '丁锦', '保健主任', '13315989238', 'teacherImgs/dingjin.jpg', null);
-INSERT INTO `teacher` VALUES ('4', '李可冉', '教学主任', '15630182128', 'teacherImgs/likeran.jpg', '爱己之心爱人，律人之心律己');
-INSERT INTO `teacher` VALUES ('5', '郭立轻', '教师', '18103397561', 'teacherImgs/guoliqing.jpg', null);
-INSERT INTO `teacher` VALUES ('6', '尹亚红', '教师', '15803216933', 'teacherImgs/yinyahong.jpg', null);
-INSERT INTO `teacher` VALUES ('7', '闫慧娟', '教师', '13403214476', 'teacherImgs/yanhuijuan.jpg', null);
-INSERT INTO `teacher` VALUES ('8', '贾佳佳', '教师', '18931187590', 'teacherImgs/jiajiajia.jpg', null);
-INSERT INTO `teacher` VALUES ('9', '孙红森', '教师', '18713138712', 'teacherImgs/sunhongsen.jpg', null);
-INSERT INTO `teacher` VALUES ('10', '康丽', '教师', '13931860066', 'teacherImgs/kangli.jpg', null);
-INSERT INTO `teacher` VALUES ('11', '索素敏', '教师', '15354215535', 'teacherImgs/suosumin.jpg', null);
-INSERT INTO `teacher` VALUES ('12', '牛金叶', '教师', '15226590277', 'teacherImgs/niujinye.jpg', null);
-INSERT INTO `teacher` VALUES ('13', '冯稳', '教师', '17732170224', 'teacherImgs/fengwen.jpg', null);
-INSERT INTO `teacher` VALUES ('14', '张宇彤', '教师', '18031920309', 'teacherImgs/zhangyutong.jpg', null);
-INSERT INTO `teacher` VALUES ('15', '张敬敏', '教师', '15100110968', 'teacherImgs/zhangjingmin.jpg', null);
-INSERT INTO `teacher` VALUES ('16', '李艳雪', '教师', '15831121240', 'teacherImgs/liyanxue.jpg', null);
-INSERT INTO `teacher` VALUES ('17', '宋勋勋', '教师', '13832165042', 'teacherImgs/songxunxun.jpg', null);
-INSERT INTO `teacher` VALUES ('18', '高静', '教师', '13184772586', 'teacherImgs/gaojing.jpg', null);
-INSERT INTO `teacher` VALUES ('19', '宋术娟', '教师', '15632305770', 'teacherImgs/songshujuan.jpg', null);
-INSERT INTO `teacher` VALUES ('20', '韩利亚', '保育员', '15531170879', 'teacherImgs/hanliya.jpg', null);
-INSERT INTO `teacher` VALUES ('21', '董焕丽', '保育员', '15631169325', 'teacherImgs/donghuanli.jpg', null);
-INSERT INTO `teacher` VALUES ('22', '倪小娜', '保育员', '13126132773', 'teacherImgs/nixiaona.jpg', null);
-INSERT INTO `teacher` VALUES ('23', '杜花英', '保育员', '18233187590', 'teacherImgs/duhuaying.jpg', null);
-INSERT INTO `teacher` VALUES ('24', '戎计梅', '保育员', '13633212136', 'teacherImgs/rongjimei.jpg', null);
-INSERT INTO `teacher` VALUES ('25', '王俊杰', '保育员', '13315989238', 'teacherImgs/wangjunjie.jpg', null);
-INSERT INTO `teacher` VALUES ('26', '刘哲', '保育员', '13933127175', 'teacherImgs/liuzhe.jpg', null);
-INSERT INTO `teacher` VALUES ('27', '刘兰', '保育员', '15303292267', 'teacherImgs/liulan.jpg', null);
+INSERT INTO `teacher` VALUES ('1', '贾静宇', '园长', '18732184631', 'teacherImgs/jiajingyu.jpg', '爱就是教育，没有爱便没有教育；不求尽善尽美，但求无愧于心。', '1');
+INSERT INTO `teacher` VALUES ('2', '周萍', '后勤主任', '15832138253', 'teacherImgs/zhouping.jpg', '用我的细心、耐心和爱心，换你的安心、放心和舒心。', '1');
+INSERT INTO `teacher` VALUES ('3', '丁锦', '保健主任', '13315989238', 'teacherImgs/dingjin.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('4', '李可冉', '教学主任', '15630182128', 'teacherImgs/likeran.jpg', '爱己之心爱人，律人之心律己', '1');
+INSERT INTO `teacher` VALUES ('5', '郭立轻', '教师', '18103397561', 'teacherImgs/guoliqing.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('6', '尹亚红', '教师', '15803216933', 'teacherImgs/yinyahong.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('7', '闫慧娟', '教师', '13403214476', 'teacherImgs/yanhuijuan.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('8', '贾佳佳', '教师', '18931187590', 'teacherImgs/jiajiajia.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('9', '孙红森', '教师', '18713138712', 'teacherImgs/sunhongsen.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('10', '康丽', '教师', '13931860066', 'teacherImgs/kangli.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('11', '索素敏', '教师', '15354215535', 'teacherImgs/suosumin.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('12', '牛金叶', '教师', '15226590277', 'teacherImgs/niujinye.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('13', '冯稳', '教师', '17732170224', 'teacherImgs/fengwen.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('14', '张宇彤', '教师', '18031920309', 'teacherImgs/zhangyutong.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('15', '张敬敏', '教师', '15100110968', 'teacherImgs/zhangjingmin.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('16', '李艳雪', '教师', '15831121240', 'teacherImgs/liyanxue.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('17', '宋勋勋', '教师', '13832165042', 'teacherImgs/songxunxun.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('18', '高静', '教师', '13184772586', 'teacherImgs/gaojing.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('19', '宋术娟', '教师', '15632305770', 'teacherImgs/songshujuan.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('20', '韩利亚', '保育员', '15531170879', 'teacherImgs/hanliya.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('21', '董焕丽', '保育员', '15631169325', 'teacherImgs/donghuanli.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('22', '倪小娜', '保育员', '13126132773', 'teacherImgs/nixiaona.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('23', '杜花英', '保育员', '18233187590', 'teacherImgs/duhuaying.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('24', '戎计梅', '保育员', '13633212136', 'teacherImgs/rongjimei.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('25', '王俊杰', '保育员', '13315989238', 'teacherImgs/wangjunjie.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('26', '刘哲', '保育员', '13933127175', 'teacherImgs/liuzhe.jpg', null, '1');
+INSERT INTO `teacher` VALUES ('27', '刘兰', '保育员', '15303292267', 'teacherImgs/liulan.jpg', null, '1');
 
 -- ----------------------------
 -- Table structure for `teacher_class`

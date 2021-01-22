d34b2e7d3a1a99e2d70897e80a4daa77f2e022cc
2018011829 <370667742@qq.com>
2021-01-22 10:49:58 +0800

    lizhe

--- android/kindergarten/app/src/main/java/com/example/myapplication/main/util/ConfigUtil.java
+++ android/kindergarten/app/src/main/java/com/example/myapplication/main/util/ConfigUtil.java
@@ -4,13 +4,7 @@ public class ConfigUtil {
     // 用户手机号码
     public static String PHONE;
     //服务器地址;
-<<<<<<< HEAD
     public static final String SERVICE_ADDRESS="http://192.168.137.1:8080/kindergarten/";
     //服务器端用户头像地址
     public static final String SETVER_AVATAR = "http://192.168.137.1:8080/kindergarten/avatar/";
-=======
-    public static final String SERVICE_ADDRESS="http://192.168.1.105:8080/kindergarten/";
-    //服务器端用户头像地址
-    public static final String SETVER_AVATAR = "http://192.168.1.105:8080/kindergarten/avatars/";
->>>>>>> 301806bdb334dfbafcf934df94df428ac872767b
 }

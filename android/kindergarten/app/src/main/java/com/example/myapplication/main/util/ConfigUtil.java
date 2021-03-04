3de888a3a842f8742ecebd85b8b945ce84b9aaa2
liurufeng666 <2221336136@qq.com>
2021-03-04 20:03:23 +0800

    update

--- android/kindergarten/app/src/main/java/com/example/myapplication/main/util/ConfigUtil.java
+++ android/kindergarten/app/src/main/java/com/example/myapplication/main/util/ConfigUtil.java
@@ -4,7 +4,7 @@ public class ConfigUtil {
     // 用户手机号码
     public static String PHONE=null;
 
-    public static final String SERVICE_ADDRESS="http://192.168.1.105:8080/kindergarten/";
+    public static final String SERVICE_ADDRESS="http://121.89.209.191:8080/kindergarten/";
     //服务器端用户头像地址
-    public static final String SETVER_AVATAR = "http://192.168.1.105:8080/kindergarten/avatars/";
+    public static final String SETVER_AVATAR = "http://121.89.209.191:8080/kindergarten/avatars/";
 }
